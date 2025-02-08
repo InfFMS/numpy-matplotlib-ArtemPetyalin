a919e1e0e244df0e7ecc9f9690f1c10b1331611e
Петялин Артём Дмитриевич <a.petyalin@main.nsu.ru>
2025-02-08 14:23:52 +0700

    Здравствуйте! Сделал 4 и 5

--- wether.py
+++ wether.py
@@ -10,6 +10,7 @@
 # Подсветку "холодных" и "жарких" дней на линейном графике.
 import random
 import matplotlib.pyplot as plt
+import numpy as np
 a = [random.randint(-10, 35) for i in range(365)]
 av_temp = 0
 heat_counter = 0
@@ -41,3 +42,28 @@ for i in range(len(colds)):
         max_cold = colds[i]
 
 print(av_temp, max_cold, heat_counter)
+
+fig = plt.figure(figsize=(8, 8))
+
+ax1 = fig.add_axes([0.1, 0.5, 0.8, 0.4])
+x1 = np.linspace(1, 365, 365)
+y1 = np.array(a)
+ax1.plot(x1, y1, label="weather", color="green")
+ax1.set_title("Погода по дням года")
+
+for i in range(len(a)):
+
+    if a[i] < 0:
+        point1 = plt.Circle((i + 1, a[i]), 0.25, color="blue")
+        ax1.add_patch(point1)
+
+    elif a[i] > 25:
+        point2 = plt.Circle((i + 1, a[i]), 0.25, color="red")
+        ax1.add_patch(point2)
+
+ax2 = fig.add_axes([0.1,0.03,0.8,0.4])
+y2 = np.array(a)
+ax2.hist(y2, 46)
+ax2.set_title("Количество дней с одинаковой температурой")
+
+plt.show()
